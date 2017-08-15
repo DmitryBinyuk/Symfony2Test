7ad59a9e3ba39354500dc000de2e6a4239fc5c51
Dmitry Binyuk <dmitry.binyuk@chisw.com>
2017-08-15 13:08:58 +0300

    Added new bundle and fetch data from json file

--- src/ApiBundle/Controller/DepositStationController.php
+++ src/ApiBundle/Controller/DepositStationController.php
@@ -30,43 +30,44 @@ class DepositStationController extends Controller
 	
 	    foreach($jsonObject->collections->patient as $key=>$value){
             if(!in_array($value->label, $labels)){
-            $labels[] = $value->label;
+                $labels[] = $value->label;
             }
 
             if(!in_array($value->group, $groups)){
-            $groups[] = $value->group;
+                $groups[] = $value->group;
             }
 
             if(!in_array($value->status, $statuses)){
-            $statuses[] = $value->status;
+                $statuses[] = $value->status;
             }
 
             if(!in_array($value->details, $details)){
-            $details['email'] = $value->details->email;
-            $details['tel'] = $value->details->tel;
+                $details['email'] = $value->details->email;
+                $details['tel'] = $value->details->tel;
+	        }
 	    }
-	}
 	
-	$responseArray = [];
-	
-	$responseArray['navigation'] = json_decode(json_encode($jsonObject->navigation), true);
-	
-	for($i=1; $i<=20000; $i++){
-	    
-	    $compinedValue = ['id' => $i,
-			      'label' => $labels[rand(0, count($labels)-1)],
-			      'group' => $groups[rand(0, count($groups)-1)],
-			      'status' => $statuses[rand(0, count($statuses)-1)],
-			      'details' => ['email' => $details['email'], 'tel' => $details['tel']],
-	    		     ];
-	    
-	    $responseArray['collections']['patient'][] = $compinedValue;
-	}
+        $responseArray = [];
 
-	$response = new JsonResponse(['data' => $responseArray]);
-	$response->headers->set('Access-Control-Allow-Origin', 'http://localhost:8080');
-	
-	return $response;
+        $responseArray['navigation'] = json_decode(json_encode($jsonObject->navigation), true);
+
+        //create sequence of 20 000 elements woth random values from json file
+        for($i=1; $i<=20000; $i++){
+
+            $compinedValue = ['id' => $i,
+                      'label' => $labels[rand(0, count($labels)-1)],
+                      'group' => $groups[rand(0, count($groups)-1)],
+                      'status' => $statuses[rand(0, count($statuses)-1)],
+                      'details' => ['email' => $details['email'], 'tel' => $details['tel']],
+                         ];
+
+            $responseArray['collections']['patient'][] = $compinedValue;
+        }
+
+        $response = new JsonResponse(['data' => $responseArray]);
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+        return $response;
     }
 
 }
